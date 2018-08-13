3254bc155ce5e5e5e6af3e89586fe153dbc08207
Jeff P <jeff@siccr.com>
2018-08-13 13:44:45 -0600

    compose build and start

--- tomcat/tomcat_up.sh
+++ tomcat/tomcat_up.sh
@@ -10,7 +10,7 @@ cp /orcid/tomcat-users.min.xml $TOMCAT_HOME/conf/tomcat-users.xml
 cp /orcid/setenv.sh $TOMCAT_HOME/bin/
 #cp /orcid/orcid.properties $TOMCAT_HOME/conf/
 cp /orcid_source/orcid-persistence/src/main/resources/staging-persistence.properties $TOMCAT_HOME/conf/orcid.properties
-
+sed -i 's|localhost:5432|dev-db.orcid.org:5432|g' $TOMCAT_HOME/conf/orcid.properties
 chmod +x $TOMCAT_HOME/bin/setenv.sh
 
 echo 'CATALINA TOMCAT ready to start.'
